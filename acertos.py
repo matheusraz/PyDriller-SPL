8f9308fe7fe24e20e35fb66d93c3cbd26dfc926c
Matheus Raz <mrol@cin.ufpe.br>
2019-09-01 23:49:08 -0300

    Novos resultados com nova comparação

--- acertos.py
+++ acertos.py
@@ -1,9 +1,23 @@
 auto = open('automated-results.csv','r')
 karine = open('resultado_manual_formatado.csv','r')
 
+def separaClassificações(lista):
+    retorno = []
+    itemMontado = ''
+    for i in range(len(lista)):
+        if(lista[i] != '[' and lista[i] != ']'):
+            if(lista[i] == '|'):
+                retorno.append(itemMontado)
+                itemMontado = ''
+            else:
+                itemMontado += lista[i]
+    retorno.append(itemMontado)
+    return retorno
+
+
+
 automaticos = []
 manuais = []
-acertos = 0
 
 for lines in auto:
     val = lines.split(',')[2]
@@ -13,15 +27,27 @@ for lines in karine:
     val = lines.split(',')[2]
     manuais.append(val.replace(' ',''))
 
+
+# COMPARAÇÃO DA STRING COMPLETA
+acertos = 0
 for line in range(len(automaticos)):
-    
     if(automaticos[line].strip() == manuais[line].strip()):
         acertos += 1
-    else:
-        if(line == 3):
-            print("SOU DIFERENTE")
-            print(automaticos[line].strip())
-            print(manuais[line].strip())
+
+stringCompleta = acertos/len(automaticos)*100
+
+# COMPARAÇÃO POR SUBCONJUNTO (MANUAIS CONTIDOS EM AUTOMATICOS)
+acertos = 0
+totalTags = 0
+for line in range(len(automaticos)):
+    itensAuto = automaticos[line].strip()
+    itensManual = manuais[line].strip()
+    for i in range(len(itensManual)):
+        for j in range(len(itensAuto)):
+            if(i == j):
+                acertos += 1
+        totalTags += 1
 
 
-print('Taxa de acerto: {0:.2f}%'.format(acertos/len(automaticos)*100))
+print('Taxa de acerto por comparação total: {0:.2f}%'.format(stringCompleta))
+print('Taxa de acerto por subconjuntos: {0:.2f}%'.format(acertos/totalTags*100))
