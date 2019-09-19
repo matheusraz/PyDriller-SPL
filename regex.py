10803e8995275d60acbd2d4f36b5264f3fac5d23
Matheus Raz <mrol@cin.ufpe.br>
2019-09-19 01:31:11 -0300

    70% done for regex in makefile

--- regex.py
+++ regex.py
@@ -6,7 +6,7 @@ teste = 'depends on HAVE_NODEJS'
 st = 'select n'
 st2 = 'I would like to select any values'
 
-res = re.match(r'^select \S+', st2) # SELECT          *
+# res = re.match(r'^select \S+', st2)  SELECT           *
 # res = re.match(r'^depends on \S+', teste) DEPENDS     *
 # res = re.match(r'^default \S', teste) DEFAULT         *
 # res = re.match(r'^menu \"w+\"', st) MENU              *
@@ -14,7 +14,37 @@ res = re.match(r'^select \S+', st2) # SELECT          *
 # res = re.match(r'^bool \"w+\"', st) BOOL              *
 # res = re.match(r'^prompt \"w+\"', st) PROMPT          *
 # res = re.match(r'^option \"w+\"', st) OPTION          *
-print(res != None)
+# print(res != None)
+
+# Mapping (mapeamento)
+# ifdef
+# build
+
+# dale = 'obj-core-$(MAINLOOP_GLIB)-extra-cflags += $(GLIB_CFLAGS)'
+# res = re.match(r'^\S* := \S*', dale)
+# res2 = re.match(r'^\S* \+= \S*', dale)
+# if(res != None or res2 != None):
+#     print('MAPPING')
+
+dale = 'ifdef LINUX'
+
+res = re.match(r'^ifeq \S*', dale)
+res2 = re.match(r'^ifneq \S*', dale)
+res3 = re.match(r'^ifdef \S*', dale)
+
+if(res != None or res2 != None or res3 != None):
+    print('IFDEF')
+
+
+
+# obj-core-$(KDBUS)-extra-cflags := $(SYSTEMD_CFLAGS) -- ANTES
+                                                    # -- MappingA
+# obj-core-$(KDBUS)-extra-cflags := $(TESTE) -- DEPOIS
+
+# Mapping - X := / += Y
+
+
+
 
 
 
